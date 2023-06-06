359a8d8eae2ce5647a7b16a82938c91cb78f9658
codepanther <pezechigozirim@gmail.com>
2023-06-06 02:31:49 +0100

    Create and style 'Featured' component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import './Home.scss';
+import Featured from '../../components/featured/Featured';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <Featured />
+    </div>
   )
 }
 
