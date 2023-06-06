4e06b5adeb365ae0924fc4fe203b91ab6ba5715f
codepanther <pezechigozirim@gmail.com>
2023-06-06 02:31:14 +0100

    Add project images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,49 @@
-import avatar from '../assets/avatar.png';
+import avatar from '../assets/img/avatar.png';
+import accessibility from '../assets/img/accessibility.png';
+import check from '../assets/img/check.png';
+import clock from '../assets/img/clock.png';
+import coin from '../assets/img/coin.png';
+import deleteIcon from '../assets/img/delete.png';
+import dislike from '../assets/img/dislike.png';
+import down from '../assets/img/down.png';
+import facebook from '../assets/img/facebook.png';
+import greencheck from '../assets/img/greencheck.png';
+import heart from '../assets/img/heart.png';
+import instagram from '../assets/img/instagram.png';
+import language from '../assets/img/language.png';
+import like from '../assets/img/like.png';
+import linkedin from '../assets/img/linkedin.png';
+import man from '../assets/img/man.png';
+import message from '../assets/img/message.png';
+import pinterest from '../assets/img/pinterest.png';
+import recycle from '../assets/img/recycle.png';
+import search from '../assets/img/search.png';
+import star from '../assets/img/star.png';
+import twitter from '../assets/img/twitter.png';
 
 const images = {
-  avatar
-}
+  avatar,
+  accessibility,
+  check,
+  clock,
+  coin,
+  deleteIcon,
+  dislike,
+  down,
+  facebook,
+  greencheck,
+  heart,
+  instagram,
+  language,
+  like,
+  linkedin,
+  man,
+  message,
+  pinterest,
+  recycle,
+  search,
+  star,
+  twitter
+};
 
 export default images;
