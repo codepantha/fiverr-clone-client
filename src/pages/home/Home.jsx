5c0adbf27d1c847216e401d60bd8281a25e59205
codepanther <pezechigozirim@gmail.com>
2023-06-06 14:19:40 +0100

    Create features section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import TrustedBy from '../../components/trustedBy/TrustedBy';
 import Slide from '../../components/Slide/Slide';
 import { cards } from '../../constants/data';
 import CatCard from '../../components/catCard/CatCard';
+import images from '../../constants/images';
 
 const Home = () => {
   return (
@@ -17,6 +18,48 @@ const Home = () => {
           <CatCard key={card.id} item={card} />
         ))}
       </Slide>
+
+      <section className="features">
+        <div className="container">
+          <section className="item">
+            <h2>A whole world of freelance talent at your fingertips</h2>
+            <article>
+              <figure className="title">
+                <img src={images.check} alt="" />
+                <figcaption>The best for every budget</figcaption>
+              </figure>
+              <p>
+                Find high-quality services at every price point. No hourly
+                rates, just project-based pricing.
+              </p>
+            </article>
+            <article>
+              <figure className="title">
+                <img src={images.check} alt="" />
+                <figcaption>The best for every budget</figcaption>
+              </figure>
+              <p>
+                Find high-quality services at every price point. No hourly
+                rates, just project-based pricing.
+              </p>
+            </article>
+            <article>
+              <figure className="title">
+                <img src={images.check} alt="" />
+                <figcaption>The best for every budget</figcaption>
+              </figure>
+              <p>
+                Find high-quality services at every price point. No hourly
+                rates, just project-based pricing.
+              </p>
+            </article>
+          </section>
+
+          <section className="item">
+            <video src="../video.mp4" controls></video>
+          </section>
+        </div>
+      </section>
     </div>
   );
 };
