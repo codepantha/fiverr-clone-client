960ba23ed56bcbee6d2dbe1cd40700555ee7f5a5
codepanther <pezechigozirim@gmail.com>
2023-06-06 13:00:53 +0100

    Create category slider

---
+++ src/components/catCard/CatCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+import './CatCard.scss'
+
+const CatCard = ({ item }) => {
+  return (
+    <Link>
+      <article className="catCard">
+        <figure>
+          <img src={item.img} alt={item.title} />
+          <p className="desc">{item.desc}</p>
+          <figcaption className="title">{item.title}</figcaption>
+        </figure>
+      </article>
+    </Link>
+  )
+}
+
+export default CatCard
