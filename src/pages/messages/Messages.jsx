1cd21a487f182c4d372c7f371ca8b664270e7e7c
codepanther <pezechigozirim@gmail.com>
2023-10-30 10:02:06 +0100

    Load conversations and mark conversation as read

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,18 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Messages.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Messages.scss';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import axiosRequest from '../../utils/axiosRequest';
+import { formatDate } from '../../utils/helper';
 
 const Messages = () => {
-  const currentUser = {
-    id: 1,
-    username: "Anna",
-    isSeller: true,
-  };
+  const currentUser = JSON.parse(localStorage.getItem('currentUser')) || null;
 
-  const message = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-  maxime cum corporis esse aspernatur laborum dolorum? Animi
-  molestias aliquam, cum nesciunt, aut, ut quam vitae saepe repellat
-  nobis praesentium placeat.`;
+  const queryClient = useQueryClient();
+
+  const {
+    isLoading,
+    data: conversations,
+    error
+  } = useQuery({
+    queryKey: ['conversations'],
+    queryFn: () => axiosRequest.get('/conversations').then((res) => res.data)
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: (id) => {
+      return axiosRequest.put(`/conversations/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['conversations']);
+    }
+  });
+
+  const handleMarkAsRead = (id) => mutate(id);
 
   return (
     <div className="messages">
@@ -20,62 +36,46 @@ const Messages = () => {
         <div className="title">
           <h1>Messages</h1>
         </div>
-        <table>
-          <tr>
-            <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
-            <th>Last Message</th>
-            <th>Date</th>
-            <th>Action</th>
-          </tr>
-          <tr className="active">
-            <td>Charley Sharp</td>
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>1 hour ago</td>
-            <td>
-              <button>Mark as Read</button>
-            </td>
-          </tr>
-          <tr className="active">
-            <td>John Doe</td>
-
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>2 hours ago</td>
-            <td>
-              <button>Mark as Read</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Elinor Good</td>
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>1 day ago</td>
-          </tr>
-          <tr>
-            <td>Garner David </td>
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>2 days ago</td>
-          </tr>
-          <tr>
-            <td>Troy Oliver</td>
-            <td>{message.substring(0, 100)}</td>
-            <td>1 week ago</td>
-          </tr>
-        </table>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Something bad happened...</p>
+        ) : (
+          <table>
+            <tr>
+              <th>{currentUser.isSeller ? 'Buyer' : 'Seller'}</th>
+              <th>Last Message</th>
+              <th>Date</th>
+              <th>Action</th>
+            </tr>
+            {conversations.map((c) => (
+              <tr
+                key={c._id}
+                className={
+                  ((currentUser.isSeller && !c.readBySeller) ||
+                    (!currentUser.isSeller && !c.readByBuyer)) &&
+                  'active'
+                }
+              >
+                <td>{currentUser.isSeller ? c.sellerId : c.buyerId}</td>
+                <td>
+                  <Link to={`/messages/${c.id}`} className="link">
+                    {c?.lastMessage?.substring(0, 100)}...
+                  </Link>
+                </td>
+                <td>{formatDate(c.updatedAt)}</td>
+                <td>
+                  {((currentUser.isSeller && !c.readBySeller) ||
+                    (!currentUser.isSeller && !c.readByBuyer)) && (
+                    <button onClick={() => handleMarkAsRead(c.id)}>
+                      Mark as Read
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </table>
+        )}
       </div>
     </div>
   );
